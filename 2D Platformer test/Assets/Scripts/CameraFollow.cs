d8ed63876858bc64af36c1d02080e39af8ea51cd
JoltStorm <88452355+JoltStorm@users.noreply.github.com>
2022-08-12 00:53:23 -0500

    Update CameraFollow.cs
    Added comments

--- 2D Platformer test/Assets/Scripts/CameraFollow.cs
+++ 2D Platformer test/Assets/Scripts/CameraFollow.cs
@@ -17,5 +17,6 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3 (Player.transform.position.x + 0, 10, -21);
+        //makes the camera move to the player's position, then adds an offset
     }
 }
